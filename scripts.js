eee3a2aa88ab0284d80785990b81172a9a989c17
Pat Kelly <PatK80@gmail.com>
2020-09-29 17:18:55 -0400

    changing "var" to "let"

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-var billInput = document.querySelector("#bill");
-var tipInput = document.querySelector("#tipPrePercent")
-var button = document.querySelector("button");
-var answer = document.querySelector("#calculatedTip");
-var tipPercent = document.querySelector("#tipPercent");
+let billInput = document.querySelector("#bill");
+let tipInput = document.querySelector("#tipPrePercent")
+let button = document.querySelector("button");
+let answer = document.querySelector("#calculatedTip");
+let tipPercent = document.querySelector("#tipPercent");
 
 button.addEventListener("click", function(){
-    var bill = billInput.value;
-    var prePercent = tipInput.value;
-    var percent =  tipInput.value / 100;    
-    var tipAmount = (bill * percent).toFixed(2)
+    let bill = billInput.value;
+    let prePercent = tipInput.value;
+    let percent =  tipInput.value / 100;    
+    let tipAmount = (bill * percent).toFixed(2)
     answer.innerHTML = "$" + tipAmount
     answer.style.border = "solid green 3px";
     answer.style.padding = "10px";
